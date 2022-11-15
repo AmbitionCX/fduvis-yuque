0524158c44dd3bf35b1e5454dcf52d730acae397
AmbitionCX <chenxuanamazing@gmail.com>
2022-11-15 14:34:03 +0800

    [add] add path on frontend; add NFT contract

---
+++ onchain/contracts/erc721.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract FDUVIS is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("FDUVISer", "FVer") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafkreie53dia4z52b6wqln2chb7naw7rp4nrj5hbh4ftbkqfeinixswz54";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
--- onchain/scripts/deploy.js
+++ onchain/scripts/deploy.js
@@ -1,4 +1,4 @@
-async function main() {
+async function deploy_token() {
 
     const [deployer] = await ethers.getSigners();  
     console.log("Deploying contracts with the account:", deployer.address);
@@ -8,13 +8,26 @@ async function main() {
     const token = await Token.deploy();
   
     console.log("Token address:", token.address);
-  }
+}
+
+async function deploy_nft() {
+
+    const [deployer] = await ethers.getSigners();  
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
   
-main()
+    const NFT = await ethers.getContractFactory("FDUVIS");
+    const nft = await NFT.deploy();
+  
+    console.log("NFT address:", nft.address);
+}
+
+deploy_nft()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
         process.exit(1);
     });
 
-// contract address: 0xDAA0fB931ef3bA2335F10A3d92174CfF73c49C57
+// token address: 0xDAA0fB931ef3bA2335F10A3d92174CfF73c49C57
+// nft address: 0x6C82514Ab5b149A726c360060d4DDBD946fBeFd3
